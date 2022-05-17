a7122bcfc40668b9ba4e54c0b0c5c959e744b189
Thomas2710 <thomas.trevisan@studenti.unitn.it>
2022-05-17 10:40:42 +0200

    pagina dettaglio alloggio

--- src/index.js
+++ src/index.js
@@ -11,6 +11,17 @@ app.get('/login', function(req, res) {
 app.get('/registrazione', function(req, res) {
     res.sendFile(path.join(__dirname, './public/registrazione.html'));
   });
+
+  app.get('/alloggio', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/informazioniAlloggio.html'));
+  });
+
+app.get('/logout', function(req, res){
+    res.cookie('jwt', '', {maxAge: 1});
+    res.clearCookie('jwt');
+    res.status(200);
+    res.redirect('/home');
+});
 //app.use('/static', express.static('public'));
 app.listen(process.env.PORT || 8081, '0.0.0.0', () => {
     console.log(`Scroccotour UI server started`);
