b5cfcaa4c3fa0350e58272fb4cad311b11c7baf6
Jiarre <38040300+Jiarre@users.noreply.github.com>
2022-06-07 16:51:43 +0200

    Added redirect to login

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ const path = require('path');
   app.use(cors());
 
   app.use('/static', express.static(path.join(__dirname, 'public')))
-
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/login.html'));
+  });
   app.get('/login', function(req, res) {
       res.sendFile(path.join(__dirname, './public/login.html'));
     });
